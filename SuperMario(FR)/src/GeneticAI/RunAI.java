1d53776ed080bd5311e2f1a7a8611022526a3d78
cjthimm <cjthimm@gmail.com>
2019-12-17 03:04:17 -0600

    cleaned up runAI

--- SuperMario(FR)/src/GeneticAI/RunAI.java
+++ SuperMario(FR)/src/GeneticAI/RunAI.java
@@ -11,85 +11,75 @@ import br.ol.smb.infra.Game.GameState;
 
 public class RunAI {
 
-	private static int[] genes =null;
-	private static int organism =0;
-	private static int count = 0;
+	private static int[] genes = null;
 	private static boolean x = true;
 
 	public static void setGenes(int[] genes2) {
 		genes = genes2;
 	}
-	public static void NextGeneration() {
-		organism = 0;
-	}
-	
+
 	public static void NextMario() {
-		System.out.println("Move to next organism");
-		organism++;
 		x = false;
 	}
 
- 
 	public static int runRobot(Organism o) {
 		int count = 0;
-		x= true;
+		x = true;
 		setGenes(o.genotype.genes);
-		//genes = GeneGenerator.CreateGenes();
 		int fitness = -1;
 		Robot r;
-		
-		//int preorg = 0;
+
 		try {
 			r = new Robot();
-			//r.delay(4000);
-			//r.delay(15000);
-			while(Game.getGameState() != GameState.PLAYING) {r.delay(4000*60/Time.fps);}
-			 while (x) {
-				 
-				 
-				 //System.out.println("Generation number" + GeneticAlgorithm.generationNumber+ " Organism #: "+ organism);
-				 
-				 //if(genes[count][organism] == 0) {
-				 if(genes[count] == 0) {
+			// r.delay(4000);
+			// r.delay(15000);
+			while (Game.getGameState() != GameState.PLAYING) {
+				r.delay(4000 * 60 / Time.fps);
+			}
+			while (x) {
+
+				// System.out.println("Generation number" + GeneticAlgorithm.generationNumber+ "
+				// Organism #: "+ organism);
+
+				if (genes[count] == 0) {
 					r.keyPress(KeyEvent.VK_RIGHT);
-				 	r.delay(250*60/Time.fps);
+					r.delay(250 * 60 / Time.fps);
 					r.keyRelease(KeyEvent.VK_RIGHT);
-					//System.out.println("right - "+(250*60/Time.fps));
-				}
-				
-				if(genes[count] == 1) {
-					r.keyPress(KeyEvent.VK_RIGHT);
-					r.keyPress(KeyEvent.VK_X);
-					r.delay(350*60/Time.fps);
-					r.keyRelease(KeyEvent.VK_RIGHT);
-					r.keyPress(KeyEvent.VK_X);
-					//System.out.println("jump - "+(350*60/Time.fps));
-				}
-				
-				if(genes[count] == 2) {
-					r.keyPress(KeyEvent.VK_X);
-					r.delay(350*60/Time.fps);
-					r.keyRelease(KeyEvent.VK_X);
-					//System.out.println("jump - "+(350*60/Time.fps));
-				}
-				
-				if(genes[count] == 3) {
-					r.keyPress(KeyEvent.VK_Z);
-					r.delay(130*60/Time.fps);
-					r.keyRelease(KeyEvent.VK_Z);
-					//System.out.println("FireBALL!! - "+(130*60/Time.fps));
+					// System.out.println("right - "+(250*60/Time.fps));
 				}
 
-				//r.delay(100*60/Time.fps);
-				//System.out.println("r.delay - "+(100*60/Time.fps));
+				if (genes[count] == 1) {
+					r.keyPress(KeyEvent.VK_RIGHT);
+					r.keyPress(KeyEvent.VK_X);
+					r.delay(350 * 60 / Time.fps);
+					r.keyRelease(KeyEvent.VK_RIGHT);
+					r.keyPress(KeyEvent.VK_X);
+					// System.out.println("jump - "+(350*60/Time.fps));
+				}
+
+				if (genes[count] == 2) {
+					r.keyPress(KeyEvent.VK_X);
+					r.delay(350 * 60 / Time.fps);
+					r.keyRelease(KeyEvent.VK_X);
+					// System.out.println("jump - "+(350*60/Time.fps));
+				}
+
+				if (genes[count] == 3) {
+					r.keyPress(KeyEvent.VK_Z);
+					r.delay(130 * 60 / Time.fps);
+					r.keyRelease(KeyEvent.VK_Z);
+					// System.out.println("FireBALL!! - "+(130*60/Time.fps));
+				}
+
+				// r.delay(100*60/Time.fps);
+				// System.out.println("r.delay - "+(100*60/Time.fps));
 				count++;
 				fitness = Time.getxAfterDeath();
-				//System.out.println("fitness: "+fitness);
-			
+				// System.out.println("fitness: "+fitness);
+
 			}
-			 
+
 		} catch (AWTException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return fitness;
