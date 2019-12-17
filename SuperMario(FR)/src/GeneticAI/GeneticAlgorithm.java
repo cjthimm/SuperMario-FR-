f546925474425d49e1dcc18c18d1b6db3b207b85
cjthimm <cjthimm@gmail.com>
2019-12-16 18:59:27 -0600

    Cleaned up geneticAlgorithm

--- SuperMario(FR)/src/GeneticAI/GeneticAlgorithm.java
+++ SuperMario(FR)/src/GeneticAI/GeneticAlgorithm.java
@@ -1,105 +1,114 @@
 package GeneticAI;
-​import java.util.Random;
+
+​
+
+import java.util.Random;
 
 public class GeneticAlgorithm {
 
-	//String answer="levelCleared" ;
-	//casey is finding out how to represent this with x distance
-	int genotypeSize= 10;
-	int maxGenerations= 1000;
-	int fitnessThreshold=10;
-	int generationSize=100;
-	int numberOfParents=2;
-	double mutationRate=0.03;
-	
-	class Organism{
+	// String answer="levelCleared" ;
+	// casey is finding out how to represent this with x distance
+	int genotypeSize = 10;
+	int maxGenerations = 1000;
+	int fitnessThreshold = 10;
+	int generationSize = 100;
+	int numberOfParents = 2;
+	double mutationRate = 0.03;
+
+	class Organism {
 		Genotype genotype;
 		int fitness;
 	}
-	
-	public static class Genotype{
-		public static Organism CreateGenes(){
+
+	public static class Genotype {
+		public static Organism CreateGenes() {
 			int values[][] = new int[20][1200];
 			System.out.println("Creating genes");
-		    for (int i = 0; i < values.length; i++) {
-		        // do the for in the row according to the column size
-		    	System.out.println("");
-				System.out.println("Printing the genenome of of the "+i+" organism:");
-		        for (int j = 0; j < values[i].length; j++) {
-		            // multiple the random by 10 and then cast to in
-		            values[i][j] = ((int) (Math.random() * 4));
-		            System.out.print(values[i][j]);
-		        }
-		    }
-		}	
-	}
-	
-	void geneticAlgorithm() {
-		Organism[] generation= new Organism[generationSize];
-		for(int i=0; i<=generationSize;i++) {
-			generation[i]= Genotype.CreateGenes();
-			//and organism with a randomly generated genotype
-			//randomly generate java.util.random with the about genes array
+			for (int i = 0; i < values.length; i++) {
+				// do the for in the row according to the column size
+				System.out.println("");
+				System.out.println("Printing the genenome of of the " + i + " organism:");
+				for (int j = 0; j < values[i].length; j++) {
+					// multiple the random by 10 and then cast to in
+					values[i][j] = ((int) (Math.random() * 4));
+					System.out.print(values[i][j]);
+				}
+			}
 		}
-		for(int g=0; g<=maxGenerations;g++) {
-			if(generation[0].fitness >= fitnessThreshold)
+	}
+
+	void geneticAlgorithm() {
+		Organism[] generation = new Organism[generationSize];
+		for (int i = 0; i <= generationSize; i++) {
+			generation[i] = Genotype.CreateGenes();
+			// and organism with a randomly generated genotype
+			// randomly generate java.util.random with the about genes array
+		}
+		for (int g = 0; g <= maxGenerations; g++) {
+			if (generation[0].fitness >= fitnessThreshold)
 				break;
-			for(int o; o<=generation.length; o++) {
-				//******Casey******* I think you said you were going to figure out how to find this distance?
-				//take the length of the x axis and take that difference and subtract by an arbitrarily large number
-				//1000-x of mario to goal
-				//that number is not the fitness score of that organism
-				
-				int fitnessScore= evaluate(o);
-				for(int i=0; i<generation.length;i++) {
-					Organism a= generation[i];
-					Organism b= generation[i+1];
-					//swap if the organism a is smaller than b so that it will be largest to smallest
-					//obviously we cant compare the Organisms we need to give them fitness score then compare
-					if(a<b) {
+			for (int o; o <= generation.length; o++) {
+				// ******Casey******* I think you said you were going to figure out how to find
+				// this distance?
+				// take the length of the x axis and take that difference and subtract by an
+				// arbitrarily large number
+				// 1000-x of mario to goal
+				// that number is not the fitness score of that organism
+
+				int fitnessScore = evaluate(o);
+				for (int i = 0; i < generation.length; i++) {
+					Organism a = generation[i];
+					Organism b = generation[i + 1];
+					// swap if the organism a is smaller than b so that it will be largest to
+					// smallest
+					// obviously we cant compare the Organisms we need to give them fitness score
+					// then compare
+					if (a < b) {
 						Organism temp;
-						temp=a;
-						a=b;
-						b=temp;
+						temp = a;
+						a = b;
+						b = temp;
 					}
 				}
-				
+
 			}
-			
-			Organism[] parents= new Organism[numberOfParents];
-			//size of numberOfParents
-			
-			for(int i=0; i<numberOfParents;i++) {
-				//copy the array of organism over into parents in order of fitness.sorting descending
-				generation[i]=parents[i];
-				
+
+			Organism[] parents = new Organism[numberOfParents];
+			// size of numberOfParents
+
+			for (int i = 0; i < numberOfParents; i++) {
+				// copy the array of organism over into parents in order of fitness.sorting
+				// descending
+				generation[i] = parents[i];
+
 			}
-			Organism[] nextGeneration= new Organism[generationSize];
-			for(int i=0; i<=generationSize;i++) {
-				nextGeneration[i]= breed(parents);
+			Organism[] nextGeneration = new Organism[generationSize];
+			for (int i = 0; i <= generationSize; i++) {
+				nextGeneration[i] = breed(parents);
 			}
-			for(int o; o<=nextGeneration.length;o++) {
-				mutate(o);//?
+			for (int o; o <= nextGeneration.length; o++) {
+				mutate(o);// ?
 			}
-			generation= nextGeneration;
-			
+			generation = nextGeneration;
+
 		}
-		//o.fitness=number of correct characters in the Organisms genotype;//how do you find this
+		// o.fitness=number of correct characters in the Organisms genotype;//how do you
+		// find this
 	}
-	
-	Organism breed(Organism[]parents) {
-		char[]genes= new char[genotypeSize];
-		Random r= new Random();
-		for(int i=0; i<=genotypeSize;i++) {
-			
-			Organism parentToInheritFrom= parent[r.nextInt(numberOfParents)];
-			genes[i]= parentToInheritFrom.genotype.genes[i];
+
+	Organism breed(Organism[] parents) {
+		char[] genes = new char[genotypeSize];
+		Random r = new Random();
+		for (int i = 0; i <= genotypeSize; i++) {
+
+			Organism parentToInheritFrom = parent[r.nextInt(numberOfParents)];
+			genes[i] = parentToInheritFrom.genotype.genes[i];
 		}
 		return new Genotype(genes);
-		//we need a genotype constructor
-	
+		// we need a genotype constructor
+
 	}
-	
+
 	void mutate(Organism o) {
 		for(int i=0; i<=genotypeSize; i++) {
 			if(Math.random()< mutationRate) {
@@ -109,8 +118,5 @@ public class GeneticAlgorithm {
 				
 		}
 	}
-	
-		
 
-
-}//end GenAlgo class
+}// end GenAlgo class
